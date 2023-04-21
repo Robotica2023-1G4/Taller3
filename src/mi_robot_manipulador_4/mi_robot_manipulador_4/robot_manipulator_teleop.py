1bfd021a214771ec75c76333a6fe5ea6997a456d
Alejandro1400 <a.salgadom@uniandes.edu.co>
2023-04-21 14:35:43 -0500

    h

--- src/mi_robot_manipulador_4/mi_robot_manipulador_4/robot_manipulator_teleop.py
+++ src/mi_robot_manipulador_4/mi_robot_manipulador_4/robot_manipulator_teleop.py
@@ -1,6 +1,17 @@
-def main():
-    print('Hi from mi_robot_manipulador_4.')
+import rclpy
+import time
+import sys, select, tty, termios
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from pynput import keyboard
+from pynput.keyboard import Key, Listener
 
 
-if __name__ == '__main__':
-    main()
+#Solicitamos las variables de velocidad 
+velx = input("Velocidad en x (Valor entre 1 y 10): ")
+vely = input("Velocidad en y (Valor entre 1 y 10): ")
+velg = input("Velocidad garra (Valor entre 1 y 10): ")
+
+velx = float(velx)
+vely = float(vely)
+
